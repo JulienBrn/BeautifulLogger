02dd49f6aeca1f5885fc5b154d58aa1b905034b0
Julien Braine <julienbraine@yahoo.fr>
2023-01-18 15:55:12 +0100

    rm focus

--- Examples/simple_usage.py
+++ Examples/simple_usage.py
@@ -2,13 +2,11 @@ import logging
 import beautifullogger
 
 beautifullogger.setup_beautiful_logging()
-# beautifullogger.setColor(logging.DEBUG,"magenta", "on_red", attrs=["bold", "reverse", "blink"] )
 logger=logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 logger.debug("test debug")
 logger.info("test info")
-logger.focus("test focus")
 logger.warning("test warning")
 logger.error("test error")
 logger.critical("test critical")
